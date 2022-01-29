a2c99e7acdb11cf17b9fb2d8df570b7daf56b526
Andriw Fonseca <filipefonsequinha@gmail.com>
2022-01-29 15:29:03 -0300

    Update index.php
    Atualização na forma de leitura dos dados

--- index.php
+++ index.php
@@ -1,26 +1,31 @@
 <?php 
 require 'config.php';
+
+$lista = [];
+$sql = $pdo->query("SELECT * FROM usuarios");
+if($sql->rowCount() > 0){
+    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
+
 <a href="adicionar.php">ADICIONAR USUÁRIO</a>
+
 <table border="1" width="100%">
     <tr>
         <th>ID</th>
         <th>NOME</th>
         <th>E-MAIL</th>
         <th>AÇÕES</th>
-    </tr>
-    <?php
-    $sql = "SELECT * FROM usuarios";
-    $sql = $pdo->query($sql);
-    if($sql->rowCount() > 0){
-        foreach($sql->fetchAll() as $usuario){
-            echo '<tr>';
-            echo '<td>'.$usuario['id'].'</td>';
-            echo '<td>'.$usuario['nome'].'</td>';
-            echo '<td>'.$usuario['email'].'</td>';
-            echo '<td><a href="editar.php?id='.$usuario['id'].'">Editar</a> - <a href="excluir.php?id='.$usuario['id'].'">Excluir</a></td>';
-            echo '</tr>';
-        }
-    } 
-    ?>
+    </tr>   
+    <?php foreach($lista as $usuario): ?>
+        <tr>
+            <td><?php echo $usuario['id']; ?></td>
+            <td><?php echo $usuario['nome']; ?></td>
+            <td><?php echo $usuario['email']; ?></td>
+            <td>
+                <a href="editar.php?id=<?php echo $usuario['id']; ?>">[Editar] </a> 
+                <a href="excluir.php?id=<?php echo $usuario['id']; ?>">[Excluir]</a>
+            </td>
+        </tr>            
+    <?php endforeach; ?>
 </table>
